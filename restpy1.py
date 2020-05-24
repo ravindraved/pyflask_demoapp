b8136d3c4c7c614d78a65410679f2e0ac0900c2b
Ravi <rved76@gmail.com>
2020-05-24 08:05:19 +0530

    Design the REST API Endpoints

--- restpy1.py
+++ restpy1.py
@@ -2,17 +2,34 @@ from flask import Flask
 
 app = Flas(__name__)
 
-#post
-
-# Get /store
-
-# Get /store/<string:name>
-
-# GEt /store/<string:name>/item
 
 # post /store data: {name: }
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
 
 # post /store/<string:name>/item
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+# Get /store
+@app.route('/store')
+def get_stores():
+    pass
+
+
+# Get /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store():
+    pass
+
+
+# GEt /store/<string:name>/item
+@app.route('/store/<string:store_name>/item')
+def get_items_in_store(store_name):
+    pass
 
 app.run(port=5001)
-
