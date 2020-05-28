1eabb5761c44df8ffb3d1b88a32eb78f8bda8c4a
Ravi <rved76@gmail.com>
2020-05-29 01:16:45 +0530

    Using Lambda to optimize code !

--- rest-apps/flask-restful-1.py
+++ rest-apps/flask-restful-1.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
+"""
+check u are in right env - conda env list => pflask
+pip install flask, flask-restful , flask-jwt, 
+"""
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -10,16 +15,14 @@ items = []
 class Item(Resource):
     @staticmethod
     def get(name):
-        for item in items:
-            print(item)
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404
 
     @staticmethod
     def post(name):
-        # force=True will set content/Type to JSON .. silent=True will return None
-        data = request.get_json(force=True)
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {"message": f"an item with name {name} already exist"}, 400
+        data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         print(item)
