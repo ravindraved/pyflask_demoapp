7d034831f6b57ae7134e718868bb11afab97790d
Ravi <rved76@gmail.com>
2020-05-30 00:54:49 +0530

    implement put and delete method.. remember to put headers like "Content-Type","application/json"  and  when required if method annotated with @jwt_required() then has to ahve header-> "Authorization","JWT long_jwt_token" in Postman else u would get cryptic internal error!!
    missing content-type header causes request.get_json() to fail without error but then object value is None

--- rest-apps/flask-restfyl-2-jwt.py
+++ rest-apps/flask-restfyl-2-jwt.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from flask_jwt import JWT
-
-
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 """
 check u are in right env - conda env list => pflask
@@ -17,22 +16,23 @@ to encrypt data ..required by jwt obfcustin .. u need secret_key
 
 """
 app = Flask(__name__)
-app.secret_key= 'sec_key_random_here'
+app.secret_key = 'sec_key_random_here'
 api = Api(app)
 
-jwt = JWT(app,authenticate,identity)
+# using flask_jwt package, and we created two methods authenticate and identity in security module.
+jwt = JWT(app, authenticate, identity)
+# flask auto creates and /auth endpoint for us whic requres username and password that is then used to create JWT token
 
 items = []
 
 
 class Item(Resource):
-    @staticmethod
-    def get(name):
+    @jwt_required()
+    def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item is not None else 404
 
-    @staticmethod
-    def post(name):
+    def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {"message": f"an item with name {name} already exist"}, 400
         data = request.get_json()
@@ -40,6 +40,25 @@ class Item(Resource):
         items.append(item)
         print(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'item deleted'}
+
+    def put(self, name):
+        print("---")
+        data = request.get_json()
+        print("###")
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        print("item->", item)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            print("data->", data)
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
