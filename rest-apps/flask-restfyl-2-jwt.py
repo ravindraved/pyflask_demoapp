5cb5e771e7ffc958d7a236b209dac3ea1a0e29cf
Ravi <rved76@gmail.com>
2020-05-30 10:09:04 +0530

    Cleanup

--- rest-apps/flask-restfyl-2-jwt.py
+++ rest-apps/flask-restfyl-2-jwt.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api,reqparse
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required, current_identity
 from security import authenticate, identity
 
@@ -17,7 +17,7 @@ to encrypt data ..required by jwt obfcustin .. u need secret_key
 """
 app = Flask(__name__)
 app.secret_key = 'sec_key_random_here'
-app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
 api = Api(app)
 
 # using flask_jwt package, and we created two methods authenticate and identity in security module.
@@ -29,12 +29,13 @@ items = []
 
 class Item(Resource):
     parser = reqparse.RequestParser()
+    # does request parsing to pass only required field for further processing
+    # also acts like data validation guard rail - boundry check !
     parser.add_argument('price',
                         type=float,
                         required=True,
                         help="This field cannot be left blank!"
                         )
-
 
     @jwt_required()
     def get(self, name):
@@ -44,29 +45,23 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {"message": f"an item with name {name} already exist"}, 400
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         print(item)
         return item, 201
 
+    @jwt_required()
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'item deleted'}
 
     def put(self, name):
-        # does request parsing to pass only required field for further processing
-        # also acts like data validation guard rail - boundry check !
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-                            type=float,
-                            required=True,
-                            help="This field cannot be empty"
-                            )
 
-        #data = request.get_json()
-        data = parser.parse_args()
+        # data = request.get_json()
+        data = Item.parser.parse_args()
+        # Once again, print something not in the args to verify everything works
 
         item = next(filter(lambda x: x['name'] == name, items), None)
         print("item->", item)
@@ -87,4 +82,6 @@ class ItemList(Resource):
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
-app.run(port=5010, debug=True)
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)  # important to mention debug=True
